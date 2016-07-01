b93b753f60541a4cfc231d7cc6a2c655525a5b27
guidsen <guisch95@gmail.com>
2016-07-01 17:11:55 +0200

    Added the file into the shared directory

---
+++ source/shared/configure-store.js
@@ -0,0 +1,18 @@
+import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import createLogger from 'redux-logger';
+import { routerReducer } from 'react-router-redux';
+import reducers from './reducers';
+
+const logger = createLogger();
+const rootReducer = combineReducers(Object.assign({}, reducers, {
+  routing: routerReducer
+}));
+
+const configureStore = (initialState = {}, extraMiddlewares = []) => {
+  return compose(
+    applyMiddleware(thunkMiddleware, logger, ...extraMiddlewares)
+  )(createStore)(rootReducer, initialState);
+};
+
+export default configureStore;
---
+++ source/shared/routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Router, Route } from 'react-router';
+import App from './components/App';
+import OtherPage from './components/OtherPage';
+
+export default (browserHistory) => {
+  return (
+    <Router history={ browserHistory }>
+      <Route path="/" component={ App } />
+      <Route path="/test" component={ OtherPage } />
+    </Router>
+  );
+};
